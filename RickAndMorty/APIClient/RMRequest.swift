a37de622f69a9fc1c401b68b9ab30c39cad4ecbc
Itamar <iaxelrad@gmail.com>
2024-04-28 15:40:51 +0300

    Added docstrings and API related files

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Itamar Axelrad on 28/04/2024.
+//
+
+import Foundation
+
+/// Object that represents a single API Call
+final class RMRequest {
+    
+}
---
+++ RickAndMorty/APIClient/RMService.swift
@@ -0,0 +1,26 @@
+//
+//  RMService.swift
+//  RickAndMorty
+//
+//  Created by Itamar Axelrad on 28/04/2024.
+//
+
+import Foundation
+
+/// Primary API service object to get Rick and Morty data
+final class RMService {
+    
+    /// Shared singleton instance
+    static let shared = RMService()
+    
+    /// Privatized constructor
+    private init() {}
+    
+    /// Send Rick and Morty API Call
+    /// - Parameters:
+    ///   - request: Request Instance
+    ///   - completion: Callback with data or error
+    public func execute(_ request: RMRequest, completion: @escaping() -> Void) {
+
+    }
+}
