4240a4701f550e4d78a73641a324a642674f6128
Itamar <iaxelrad@gmail.com>
2024-04-29 18:10:27 +0300

    Character List View initial implementation

---
+++ RickAndMorty/Resources/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  RickAndMorty
+//
+//  Created by Itamar Axelrad on 29/04/2024.
+//
+
+import UIKit
+
+extension UIView {
+    func addSubviews(_ views:UIView...) {
+        views.forEach({
+            addSubview($0)
+        })
+    }
+}
