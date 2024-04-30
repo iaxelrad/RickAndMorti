51dee3d382c07ce45f38f2b36d6dd64f7c180d68
Itamar <iaxelrad@gmail.com>
2024-04-30 18:00:19 +0300

    Initial build of the Character Detail Screen

--- RickAndMorty/Views/RMCharacterListView.swift
+++ RickAndMorty/Views/RMCharacterListView.swift
@@ -7,8 +7,17 @@
 
 import UIKit
 
+protocol RMCharacterListViewDelegate: AnyObject {
+    func rmCharacterListView(
+        _ characterListView: RMCharacterListView,
+        didSelectCharacter character: RMCharacter
+    )
+}
+
 /// View that handles showing list of characters, loader, etc.
 final class RMCharacterListView: UIView {
+
+    public weak var delegate: RMCharacterListViewDelegate?
 
     private let viewModel = RMCharacterListViewViewModel()
 
@@ -69,6 +78,10 @@ final class RMCharacterListView: UIView {
 }
 
 extension RMCharacterListView: RMCharacterListViewViewModelDelegate {
+    func didSelectCharacter(_ character: RMCharacter) {
+        delegate?.rmCharacterListView(self, didSelectCharacter: character)
+    }
+
     func didLoadInitialCharacters() {
         spinner.stopAnimating()
         collectionView.isHidden = false
