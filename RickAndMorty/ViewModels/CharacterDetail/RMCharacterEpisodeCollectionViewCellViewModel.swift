437468293e163ec0e450e8c0235453fbdd256d14
Itamar <iaxelrad@gmail.com>
2024-05-03 19:31:53 +0300

    Improved Episode Tab design

--- RickAndMorty/ViewModels/CharacterDetail/RMCharacterEpisodeCollectionViewCellViewModel.swift
+++ RickAndMorty/ViewModels/CharacterDetail/RMCharacterEpisodeCollectionViewCellViewModel.swift
@@ -5,7 +5,7 @@
 //  Created by Itamar Axelrad on 03/05/2024.
 //
 
-import Foundation
+import UIKit
 
 protocol RMEpisodeDataRender {
     var name: String { get }
@@ -18,6 +18,8 @@ final class RMCharacterEpisodeCollectionViewCellViewModel: Hashable, Equatable {
     private var isFetching = false
     private var dataBlock: ((RMEpisodeDataRender) -> Void)?
 
+    public let borderColor: UIColor
+
     private var episode: RMEpisode? {
         didSet {
             guard let model = episode else {
@@ -29,8 +31,9 @@ final class RMCharacterEpisodeCollectionViewCellViewModel: Hashable, Equatable {
 
     // MARK: - Init
 
-    init (episodeDataUrl: URL?) {
+    init (episodeDataUrl: URL?, borderColor: UIColor = .systemBlue) {
         self.episodeDataUrl = episodeDataUrl
+        self.borderColor = borderColor
     }
 
     // MARK: - Public
