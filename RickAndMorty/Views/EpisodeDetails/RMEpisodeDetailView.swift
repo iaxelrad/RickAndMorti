86cbf869c0a29a101451e9750e6836882e7af5a4
Itamar <iaxelrad@gmail.com>
2024-05-04 14:38:49 +0300

    Finish Episode Details Screen

--- RickAndMorty/Views/EpisodeDetails/RMEpisodeDetailView.swift
+++ RickAndMorty/Views/EpisodeDetails/RMEpisodeDetailView.swift
@@ -7,7 +7,16 @@
 
 import UIKit
 
+protocol RMEpisodeDetailViewDelegate : AnyObject {
+    func rmEpisodeDetailView(
+        _ detailView: RMEpisodeDetailView,
+        didSelect character: RMCharacter
+    )
+}
+
 final class RMEpisodeDetailView: UIView {
+
+    public weak var delegate: RMEpisodeDetailViewDelegate?
 
     private var viewModel: RMEpisodeDetailViewViewModel? {
         didSet {
@@ -146,6 +155,19 @@ extension RMEpisodeDetailView: UICollectionViewDelegate, UICollectionViewDataSou
 
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         collectionView.deselectItem(at: indexPath, animated: true)
+        guard let viewModel else { return }
+        let sections = viewModel.cellViewModels
+        let sectionType = sections[indexPath.section]
+
+        switch sectionType {
+        case .information:
+           break
+        case .characters:
+            guard let character = viewModel.character(at: indexPath.row) else {
+                return
+            }
+            delegate?.rmEpisodeDetailView(self, didSelect: character)
+        }
     }
 }
 
@@ -158,7 +180,7 @@ extension RMEpisodeDetailView {
         switch sections[section] {
         case .information:
             return createInfoLayout()
-        case .characters(viewModel: let viewModel):
+        case .characters:
             return createCharacterLayout()
         }
     }
@@ -173,7 +195,7 @@ extension RMEpisodeDetailView {
 
         let group = NSCollectionLayoutGroup.vertical(
             layoutSize: .init(widthDimension: .fractionalWidth(1),
-                              heightDimension: .absolute(100)),
+                              heightDimension: .absolute(80)),
             subitems: [item]
         )
 
