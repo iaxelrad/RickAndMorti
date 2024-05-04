5f2b1a96bd706938ecadd7ba589108dd733cfba8
Itamar <iaxelrad@gmail.com>
2024-05-04 13:25:30 +0300

    Episode Detail Cells

--- RickAndMorty/Views/EpisodeDetails/RMEpisodeInfoCollectionViewCell.swift
+++ RickAndMorty/Views/EpisodeDetails/RMEpisodeInfoCollectionViewCell.swift
@@ -8,5 +8,28 @@
 import UIKit
 
 final class RMEpisodeInfoCollectionViewCell: UICollectionViewCell {
+    static let cellIdentifier = "RMEpisodeInfoCollectionViewCell"
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = .secondarySystemBackground
+        setUpLayer()
+    }
+
+    private func setUpLayer() {
+        layer.cornerRadius = 8
+        layer.masksToBounds = true
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.secondaryLabel.cgColor
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+
+    func configure(with viewModel: RMEpisodeInfoCollectionViewCellViewModel) {}
 }
