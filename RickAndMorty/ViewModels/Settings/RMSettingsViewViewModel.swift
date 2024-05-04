b314c1c13b5deb24b212574fcbbf4fae46c0c1f0
Itamar <iaxelrad@gmail.com>
2024-05-04 22:29:55 +0300

    Setting ViewModels

---
+++ RickAndMorty/ViewModels/Settings/RMSettingsViewViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RMSettingsViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by Itamar Axelrad on 04/05/2024.
+//
+
+import Foundation
+
+struct RMSettingsViewViewModel {
+    let cellViewModels: [RMSettingsCellViewModel]
+}
