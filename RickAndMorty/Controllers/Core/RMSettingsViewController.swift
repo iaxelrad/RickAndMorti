906296644193a6c6e99977974fbba8ae0d631025
Itamar <iaxelrad@gmail.com>
2024-05-05 19:32:17 +0300

    Prompt App Rating

--- RickAndMorty/Controllers/Core/RMSettingsViewController.swift
+++ RickAndMorty/Controllers/Core/RMSettingsViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Itamar Axelrad on 27/04/2024.
 //
 
+import StoreKit
 import SafariServices
 import SwiftUI
 import UIKit
@@ -59,7 +60,9 @@ final class RMSettingsViewController: UIViewController {
             let vc = SFSafariViewController(url: url)
             present(vc, animated: true)
         } else if option == .rateApp {
-            
+            if let windowScene = self.view.window?.windowScene {
+                SKStoreReviewController.requestReview(in: windowScene)
+            }
         }
     }
 }
