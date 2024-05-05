29afe02f296897cf44734e30f9754510fe47683c
Itamar <iaxelrad@gmail.com>
2024-05-05 19:24:14 +0300

    Tap Setting Options

--- RickAndMorty/Controllers/Core/RMSettingsViewController.swift
+++ RickAndMorty/Controllers/Core/RMSettingsViewController.swift
@@ -5,21 +5,14 @@
 //  Created by Itamar Axelrad on 27/04/2024.
 //
 
+import SafariServices
 import SwiftUI
 import UIKit
 
 /// Controller to show various app options and settings
 final class RMSettingsViewController: UIViewController {
 
-    private let settingsSwiftUIController = UIHostingController(
-        rootView: RMSettingsView(
-            viewModel: RMSettingsViewViewModel(
-                cellViewModels: RMSettingsOption.allCases.compactMap({
-                    return RMSettingsCellViewModel(type: $0)
-                })
-            )
-        )
-    )
+    private var settingsSwiftUIController: UIHostingController<RMSettingsView>?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +23,17 @@ final class RMSettingsViewController: UIViewController {
     }
 
     private func addSwiftUIController() {
+        let settingsSwiftUIController = UIHostingController(
+            rootView: RMSettingsView(
+                viewModel: RMSettingsViewViewModel(
+                    cellViewModels: RMSettingsOption.allCases.compactMap({
+                        return RMSettingsCellViewModel(type: $0) { [weak self] option in
+                            self?.handleTap(option: option)
+                        }
+                    })
+                )
+            )
+        )
         addChild(settingsSwiftUIController)
         settingsSwiftUIController.didMove(toParent: self)
 
@@ -42,5 +46,20 @@ final class RMSettingsViewController: UIViewController {
             settingsSwiftUIController.view.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
             settingsSwiftUIController.view.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
         ])
+
+        self.settingsSwiftUIController = settingsSwiftUIController
+    }
+
+    private func handleTap(option: RMSettingsOption) {
+        guard Thread.current.isMainThread else {
+            return
+        }
+
+        if let url = option.targetUrl {
+            let vc = SFSafariViewController(url: url)
+            present(vc, animated: true)
+        } else if option == .rateApp {
+            
+        }
     }
 }
